fd2319d691e0a6c202f205af4dfa5918163e01bc
Arathi-k-v <arathi@signroots.com>
2026-08-03 11:53:24 +0530

    Update ContactFormOne.tsx

--- src/components/form/ContactFormOne.tsx
+++ src/components/form/ContactFormOne.tsx
@@ -17,7 +17,7 @@ export default function ContactFormOne() {
     e.preventDefault();
 
     try {
-    const response = await fetch("http://localhost:5000/contact", {
+    const response = await fetch("https://ansar-contact-api.onrender.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
